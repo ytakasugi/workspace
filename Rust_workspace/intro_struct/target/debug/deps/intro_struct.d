78b6987ea2a1e95d4e83213c61adae0176119ba4
ytakasugi <sh7.tibi0129@gmail.com>
2020-11-22 00:20:50 +0900

    add cargo project

---
+++ Rust_workspace/intro_struct/target/debug/deps/intro_struct.d
@@ -0,0 +1,5 @@
+C:\Users\sigur\Documents\900_github\700_init\Rust_workspace\intro_struct\target\debug\deps\intro_struct.exe: src\main.rs
+
+C:\Users\sigur\Documents\900_github\700_init\Rust_workspace\intro_struct\target\debug\deps\intro_struct.d: src\main.rs
+
+src\main.rs:
