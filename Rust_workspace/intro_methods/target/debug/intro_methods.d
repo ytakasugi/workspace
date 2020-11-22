04d822118da522cacf9c845e4b6d875469209c81
ytakasugi <sh7.tibi0129@gmail.com>
2020-11-22 17:17:12 +0900

    new cargo project

---
+++ Rust_workspace/intro_methods/target/debug/intro_methods.d
@@ -0,0 +1,1 @@
+C:\Users\sigur\Documents\900_github\700_init\Rust_workspace\intro_methods\target\debug\intro_methods.exe: C:\Users\sigur\Documents\900_github\700_init\Rust_workspace\intro_methods\src\main.rs
