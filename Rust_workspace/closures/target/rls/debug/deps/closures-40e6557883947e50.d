e1509685910094559d664b102ab6c1dc2c0cc556
ytakasugi <sh7.tibi0129@gmail.com>
2020-11-23 11:08:37 +0900

    new cargo project

---
+++ Rust_workspace/closures/target/rls/debug/deps/closures-40e6557883947e50.d
@@ -0,0 +1,5 @@
+c:\Users\sigur\Documents\900_github\700_init\Rust_workspace\closures\target\rls\debug\deps\closures-40e6557883947e50.rmeta: src\main.rs
+
+c:\Users\sigur\Documents\900_github\700_init\Rust_workspace\closures\target\rls\debug\deps\closures-40e6557883947e50.d: src\main.rs
+
+src\main.rs:
---
+++ Rust_workspace/shipping_charges/target/debug/deps/shipping_charges.d
@@ -0,0 +1,5 @@
+C:\Users\sigur\Documents\900_github\700_init\Rust_workspace\shipping_charges\target\debug\deps\shipping_charges.exe: src\main.rs
+
+C:\Users\sigur\Documents\900_github\700_init\Rust_workspace\shipping_charges\target\debug\deps\shipping_charges.d: src\main.rs
+
+src\main.rs:
