3f5919da8e7d7e1b113446e6098df07aacb7958f
ytakasugi <sh7.tibi0129@gmail.com>
2020-11-23 12:48:11 +0900

    new cargo project

--- Rust_workspace/closures/src/main.rs
+++ Rust_workspace/closures/src/main.rs
@@ -1,3 +1,20 @@
+/*use std::std::thread;
+use std::time::Duration;
+
+fn simulated_expensive_calculation(intensity: u32) -> u32 {
+    // Calculate slowly
+    println!("calculation slowly...");
+    thread::sleep(Duraiton::from_secs(2));
+    intensity
+}
+*/
 fn main() {
-    println!("Hello, world!");
+    /*let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(
+        simulated_user_specified_value,
+        simulated_random_number
+    );
+    */
 }
