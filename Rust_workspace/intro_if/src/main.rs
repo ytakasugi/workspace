d68c48f2305d5fcb567f5b8070637595b4b14f55
ytakasugi <sh7.tibi0129@gmail.com>
2020-11-26 21:47:34 +0900

    move dir

--- Rust_workspace/intro_if/src/main.rs
+++ Rust_workspace/intro_if/src/main.rs
@@ -1,19 +1,31 @@
 use std::io;
 
 fn main() {
-    println!("Please enter a n");
+    println!("Please enter a n1");
 
-    let n = get_input();
+    let n1 = get_input();
 
-    if n < 0 {
-        println!("{} is negative", n);
-    } else if n > 0 {
-        println!("{} is positive", n);
+    if n1 < 0 {
+        println!("{} is negative", n1);
+    } else if n1 > 0 {
+        println!("{} is positive", n1);
     } else {
-        print!("{} is zero", n)
+        print!("{} is zero", n1)
     }
 
-    
+    println!("Please enter a n2");
+    let n2 = get_input();
+
+    if n2 < 10 && n2 > -10 {
+        println!(", and is a small number, increase ten-fold");
+        n2 * 10
+    } else {
+        println!(", and is a big number, halve the number");
+        n2 / 2
+    };
+
+    println!("{} -> {}", n1, n2);
+
 }
 
 fn get_input() -> i32 {
