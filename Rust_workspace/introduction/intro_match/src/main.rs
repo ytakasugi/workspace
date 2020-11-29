26b2905e7c0da0ba410c66dd19ac272222c66b51
ytakasugi <sh7.tibi0129@gmail.com>
2020-11-29 23:08:48 +0900

    add code

--- Rust_workspace/introduction/intro_match/src/main.rs
+++ Rust_workspace/introduction/intro_match/src/main.rs
@@ -1,4 +1,21 @@
+fn age() -> u32 {
+    15
+}
+
 fn main() {
+    println!("Tell me what type of person you are");
+
+    match age() {
+        0 => println!("I'm not born yet I guess"),
+        // Could `match` 1 ..= 12 directly but then what age
+        // would the child be? Instead, bind to `n` for the
+        // sequence of 1 ..= 12. Now the age can be reported.
+        n @ 1  ..= 12 => println!("I'm a child of age {:?}", n),
+        n @ 13 ..= 19 => println!("I'm a teen of age {:?}", n),
+        // Nothing bound. Return the result.
+        n => println!("I'm an old person of age {:?}", n),
+    }
+
     let number = 13;
 
     println!("Tell me about {}", number);
@@ -22,4 +39,13 @@ fn main() {
     };
     println!("{} -> {}", boolean, binary);
 
+    let pair = (2, -2);
+    println!("Tell me about {:?}", pair);
+
+    match pair {
+        (x, y) if x == y => println!("These are twins"),
+        (x, y) if x + y == 0 => println!("Antimatter, kaboom!"),
+        (x, _) if x % 2 == 1 => println!("The first one is odd"),
+        _ => println!("No correlation..."),
+    }
 }
