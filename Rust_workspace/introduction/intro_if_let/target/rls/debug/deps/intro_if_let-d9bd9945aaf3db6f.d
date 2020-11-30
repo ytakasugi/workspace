fe92bf6a6db4d07f2a49f2403b07979140c3ef44
ytakasugi <sh7.tibi0129@gmail.com>
2020-11-30 23:03:09 +0900

    new cargo project

---
+++ Rust_workspace/introduction/intro_if_let/target/rls/debug/deps/intro_if_let-d9bd9945aaf3db6f.d
@@ -0,0 +1,5 @@
+c:\Users\sigur\Documents\900_github\000_lerning\Rust_workspace\introduction\intro_if_let\target\rls\debug\deps\intro_if_let-d9bd9945aaf3db6f.rmeta: src\main.rs
+
+c:\Users\sigur\Documents\900_github\000_lerning\Rust_workspace\introduction\intro_if_let\target\rls\debug\deps\intro_if_let-d9bd9945aaf3db6f.d: src\main.rs
+
+src\main.rs:
