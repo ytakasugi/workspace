a13cf1b8e19b0cce0d82cf0f68aee75c893e12e1
ytakasugi <sh7.tibi0129@gmail.com>
2020-12-05 12:52:29 +0900

    changes

--- Rust_workspace/RPL/closures/target/rls/debug/deps/closures-8f731590f3877b61.d
+++ Rust_workspace/RPL/closures/target/rls/debug/deps/closures-8f731590f3877b61.d
@@ -1,5 +1,5 @@
-c:\Users\sigur\Documents\900_github\700_init\Rust_workspace\closures\target\rls\debug\deps\closures-8f731590f3877b61.rmeta: src\main.rs
+e:\000_GitHub\workspace\Rust_workspace\RPL\closures\target\rls\debug\deps\closures-8f731590f3877b61.rmeta: src\main.rs
 
-c:\Users\sigur\Documents\900_github\700_init\Rust_workspace\closures\target\rls\debug\deps\closures-8f731590f3877b61.d: src\main.rs
+e:\000_GitHub\workspace\Rust_workspace\RPL\closures\target\rls\debug\deps\closures-8f731590f3877b61.d: src\main.rs
 
 src\main.rs:
