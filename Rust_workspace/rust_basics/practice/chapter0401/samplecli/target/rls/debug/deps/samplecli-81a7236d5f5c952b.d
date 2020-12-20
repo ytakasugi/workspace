68f1a33389056b3639dae2e03943f56cb0165ad8
ytakasugi <sh7.tibi0129@gmail.com>
2020-12-20 09:32:41 +0900

    rename dir

--- Rust_workspace/rust_basics/practice/chapter0401/samplecli/target/rls/debug/deps/samplecli-81a7236d5f5c952b.d
+++
@@ -1,5 +0,0 @@
-c:\Users\sigur\Documents\900_github\000_lerning\Rust_workspace\rust_basics\practice\chapter0401\samplecli\target\rls\debug\deps\samplecli-81a7236d5f5c952b.rmeta: src\main.rs
-
-c:\Users\sigur\Documents\900_github\000_lerning\Rust_workspace\rust_basics\practice\chapter0401\samplecli\target\rls\debug\deps\samplecli-81a7236d5f5c952b.d: src\main.rs
-
-src\main.rs:
--- Rust_workspace/rust_basics/practice/chapter0401/samplecli/target/rls/debug/deps/samplecli-a8b584ffa7ea2a90.d
+++
@@ -1,5 +0,0 @@
-c:\Users\sigur\Documents\900_github\000_lerning\Rust_workspace\rust_basics\practice\chapter0401\samplecli\target\rls\debug\deps\samplecli-a8b584ffa7ea2a90.rmeta: src\main.rs
-
-c:\Users\sigur\Documents\900_github\000_lerning\Rust_workspace\rust_basics\practice\chapter0401\samplecli\target\rls\debug\deps\samplecli-a8b584ffa7ea2a90.d: src\main.rs
-
-src\main.rs:
