7513053bd35a36a10d04be2f6f18ed3070484751
ytakasugi <sh7.tibi0129@gmail.com>
2020-12-20 19:26:18 +0900

    Code fix

--- Rust_workspace/rust_basics/practice/chapter04/samplecli/src/main.rs
+++ Rust_workspace/rust_basics/practice/chapter04/samplecli/src/main.rs
@@ -1,38 +1,29 @@
-use clap::{App, Arg};
+use clap::Clap;
 
-fn main() {
-    let matches = App::new("My RPN program")
-    .version("1.0.0")
-    .author("Yusuke Takasugi")
-    .about("Super awesome sample RPN calculator")
-    .arg(
-        Arg::new("formula_file")
-        .about("Formulas wirtten in RPN")
-        .value_name("FILE")
-        .index(1)
-        .required(false),
-    )
-    .arg(
-        Arg::new("verbose")
-        .about("Sets the level of varbosity")
-        .short('v')
-        .long("verbose")
-        .required(false),
-    )
-    .get_matches();
+#[derive(Clap, Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "Your name",
+    about = "Super awesome sample RPN calculator"
+)]
 
+struct Opts {
+    // Sets the level of verbosity.
+    #[clap(short, long)]
+    verbose: bool,
 
-    match matches.value_of("formula_file") {
-        Some(file) => println!("File specified: {}", file),
-        None  => println!("No file specified."),
-    }
-    let verbose = matches.is_present("verbose");
-    println!("Is verbosity specified?: {}", verbose);
+    // Formulas written in RPN.
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
 }
 
-/*use std::env;
-
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
-}*/
+    let opts = Opts::parse();
+
+    match opts.formula_file {
+        Some(file) => println!("File specified: {}", file),
+        None => println!("No file specified."),
+    }
+    println!("Is verbosity specified?: {}", opts.verbose);
+}
