0646fcc78c6d057787bb7cfec169ea1b0e165d6b
ytakasugi <sh7.tibi0129@gmail.com>
2020-12-22 21:14:07 +0900

    add test code

---
+++ Rust_workspace/rust_basics/practice/chapter04/samplecli/target/debug/deps/samplecli-682813e1e2176362.d
@@ -0,0 +1,5 @@
+C:\Users\sigur\Documents\900_github\000_lerning\Rust_workspace\rust_basics\practice\chapter04\samplecli\target\debug\deps\samplecli-682813e1e2176362.exe: src\main.rs
+
+C:\Users\sigur\Documents\900_github\000_lerning\Rust_workspace\rust_basics\practice\chapter04\samplecli\target\debug\deps\samplecli-682813e1e2176362.d: src\main.rs
+
+src\main.rs:
