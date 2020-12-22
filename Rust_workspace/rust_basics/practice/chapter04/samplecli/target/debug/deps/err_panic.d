7ca556e8af13b9766ae0b59fe6453fa8799324aa
ytakasugi <sh7.tibi0129@gmail.com>
2020-12-22 22:57:40 +0900

    add code

---
+++ Rust_workspace/rust_basics/practice/chapter04/samplecli/target/debug/deps/err_panic.d
@@ -0,0 +1,5 @@
+C:\Users\sigur\Documents\900_github\000_lerning\Rust_workspace\rust_basics\practice\chapter04\samplecli\target\debug\deps\err_panic.exe: src\bin\err_panic.rs
+
+C:\Users\sigur\Documents\900_github\000_lerning\Rust_workspace\rust_basics\practice\chapter04\samplecli\target\debug\deps\err_panic.d: src\bin\err_panic.rs
+
+src\bin\err_panic.rs:
