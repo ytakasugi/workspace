edc037cefb78b69a8b6a24be177d1d3b8b98e6ef
ytakasugi <sh7.tibi0129@gmail.com>
2020-12-23 21:56:06 +0900

    add code

--- Rust_workspace/rust_basics/practice/chapter04/samplecli/src/bin/err_panic.rs
+++ Rust_workspace/rust_basics/practice/chapter04/samplecli/src/bin/err_panic.rs
@@ -1,13 +1,20 @@
-fn get_int_from_file() -> i32 {
+fn get_int_from_file() -> Result<i32, String> {
     let path = "number.txt";
 
-    let num_str = std::fs::read_to_string(path).expect("failed to open file.");
+    // `?`は、Result型を返す関数で使用できる演算子であり、
+    // 「その直前の結果のResult型の値がOk(t)であればtを返し、Err(e)であればErr(e)で早期リターンして関数を終了する」という動作をする。
+    let num_str = std::fs::read_to_string(path).map_err(|e| e.to_string())?;
 
-    let ret = num_str.trim().parse::<i32>().expect("failed to parse string to a number.");
-
-    ret * 2
+    num_str // 最初は`num_str`は`&str`型
+    .trim() // 文字列の前後の空白を削除する。この時点では、型は`&str`のまま
+    .parse::<i32>() // `&str`を`i32`に変換する。結果はResult<i32, String>型
+    .map(|t| t * 2) // `parse()の結果が`Ok(t)`の場合のみ、`t * 2`を実行して`Ok(t * 2)`となる。
+    .map_err(|e| e.to_string()) // `parse()`の結果が`Err(e)の場合のみ`e`の文字列表現(`String`型)を返す
 }
 
 fn main() {
-    println!("{}", get_int_from_file());
+    match get_int_from_file() {
+        Ok(x) => println!("{}", x),
+        Err(e) => println!("{}", e),
+    }
 }
---
+++ Rust_workspace/rust_basics/practice/chapter04/samplecli/target/rls/debug/deps/err_panic-cd15a1f11c375b12.d
@@ -0,0 +1,5 @@
+c:\Users\sigur\Documents\900_github\000_lerning\Rust_workspace\rust_basics\practice\chapter04\samplecli\target\rls\debug\deps\err_panic-cd15a1f11c375b12.rmeta: src\bin\err_panic.rs
+
+c:\Users\sigur\Documents\900_github\000_lerning\Rust_workspace\rust_basics\practice\chapter04\samplecli\target\rls\debug\deps\err_panic-cd15a1f11c375b12.d: src\bin\err_panic.rs
+
+src\bin\err_panic.rs:
