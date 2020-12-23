468d7e43f3bb0d82557a679020bdfa6d28d5265b
ytakasugi <sh7.tibi0129@gmail.com>
2020-12-23 23:10:54 +0900

    new source file

--- Rust_workspace/rust_basics/practice/chapter04/samplecli/src/main.rs
+++ Rust_workspace/rust_basics/practice/chapter04/samplecli/src/main.rs
@@ -2,24 +2,6 @@ use clap::Clap;
 use std::fs::File;
 use std::io::{stdin, BufRead, BufReader};
 
-#[derive(Clap, Debug)]
-#[clap(
-    name = "My RPN program",
-    version = "1.0.0",
-    author = "Your name",
-    about = "Super awesome sample RPN calculator"
-)]
-
-struct Opts {
-    // Sets the level of verbosity.
-    #[clap(short, long)]
-    verbose: bool,
-
-    // Formulas written in RPN.
-    #[clap(name = "FILE")]
-    formula_file: Option<String>,
-}
-
 struct RpnCalculator(bool);
 
 impl RpnCalculator {
@@ -63,6 +45,24 @@ impl RpnCalculator {
             panic!("invalid syntax")
         }
     }
+}
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "Your name",
+    about = "Super awesome sample RPN calculator"
+)]
+
+struct Opts {
+    // Sets the level of verbosity.
+    #[clap(short, long)]
+    verbose: bool,
+
+    // Formulas written in RPN.
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
 }
 
 fn main() {
