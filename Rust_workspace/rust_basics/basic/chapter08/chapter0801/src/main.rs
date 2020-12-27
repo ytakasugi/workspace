961ac0d3837d3a6370369b4a54700f247ad10880
ytakasugi <sh7.tibi0129@gmail.com>
2020-12-27 21:28:39 +0900

    new cargo project

--- Rust_workspace/rust_basics/basic/chapter08/chapter0801/src/main.rs
+++ Rust_workspace/rust_basics/basic/chapter08/chapter0801/src/main.rs
@@ -1,3 +1,79 @@
+// デカルト座標
+struct CartesianCoord {
+    x: f64,
+    y: f64
+}
+
+// 極座標
+//         y
+//         ^
+//         |
+// r sin θ +   +
+//         |  / r
+//         | /
+//         |/)θ
+//         +---+--------> x
+//        O    r cos θ
+
+struct PolarCoord {
+    r: f64,
+    theta: f64,
+}
+
+// トレイトを定義
+trait Coordinates {
+    // ここに関数本体は書かない
+    fn to_cartesian(self) -> CartesianCoord;
+    fn from_cartesian(cart: CartesianCoord) -> Self;
+}
+
+// デカルト座標系はそのまま
+impl Coordinates for CartesianCoord {
+    fn to_cartesian(self) -> CartesianCoord {
+        self
+    }
+    fn from_cartesian(cart: CartesianCoord) -> Self {
+        cart
+    }
+}
+
+// 極座標系は変換が必要
+impl Coordinates for PolarCoord {
+    fn to_cartesian(self) -> CartesianCoord {
+        CartesianCoord {
+            x: self.r * self.theta.cos(),
+            y: self.r * self.theta.sin(),
+        }
+    }
+    fn from_cartesian(cart: CartesianCoord) -> Self {
+        PolarCoord {
+            r: (cart.x * cart.x * cart.y * cart.y).sqrt(),
+            theta: (cart.y / cart.x).atan(),
+        }
+    }
+}
+
+impl Coordinates for (f64, f64) {
+    fn to_cartesian(self) -> CartesianCoord {
+        CartesianCoord {
+            x: self.0,
+            y: self.1,
+        }
+    }
+    fn from_cartesian(cart: CartesianCoord) -> Self {
+        (cart.x, cart.y)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // 値を用意する
+    let point = (1.0, 1.0);
+
+    // トレイトのメソッドを呼ぶ
+    let c = point.to_cartesian();
+    println!("x = {}, y = {}", c.x, c.y);
+
+    // 同じくトレイトの関連関数を呼ぶ(後述
+    let p: PolarCoord = Coordinates::from_cartesian(c);
+    println!("r = {}, θ = {}", p.r, p.theta);
 }
---
+++ Rust_workspace/rust_basics/basic/chapter08/chapter0801/target/debug/chapter0801.d
@@ -0,0 +1,1 @@
+E:\000_GitHub\workspace\Rust_workspace\rust_basics\basic\chapter08\chapter0801\target\debug\chapter0801.exe: E:\000_GitHub\workspace\Rust_workspace\rust_basics\basic\chapter08\chapter0801\src\main.rs
