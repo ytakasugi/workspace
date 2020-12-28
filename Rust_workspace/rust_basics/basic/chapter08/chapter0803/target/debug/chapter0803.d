bcb595c1f2418875dcd8d15d22e0ce856b74504d
ytakasugi <sh7.tibi0129@gmail.com>
2020-12-28 23:45:01 +0900

    new cargo project

---
+++ Rust_workspace/rust_basics/basic/chapter08/chapter0803/target/debug/chapter0803.d
@@ -0,0 +1,1 @@
+E:\000_GitHub\workspace\Rust_workspace\rust_basics\basic\chapter08\chapter0803\target\debug\chapter0803.exe: E:\000_GitHub\workspace\Rust_workspace\rust_basics\basic\chapter08\chapter0803\src\main.rs
---
+++ Rust_workspace/rust_basics/basic/chapter08/chapter0803/target/rls/debug/deps/chapter0803-0a7951cb921b273a.d
@@ -0,0 +1,5 @@
+e:\000_GitHub\workspace\Rust_workspace\rust_basics\basic\chapter08\chapter0803\target\rls\debug\deps\chapter0803-0a7951cb921b273a.rmeta: src\main.rs
+
+e:\000_GitHub\workspace\Rust_workspace\rust_basics\basic\chapter08\chapter0803\target\rls\debug\deps\chapter0803-0a7951cb921b273a.d: src\main.rs
+
+src\main.rs:
