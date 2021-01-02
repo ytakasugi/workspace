5f935ba644f0a5a37508cb1a74e5978b39d2801b
ytakasugi <sh7.tibi0129@gmail.com>
2021-01-02 18:03:58 +0900

    new cargo project and change file

--- Rust_workspace/readme.txt
+++ Rust_workspace/readme.txt
@@ -1,5 +1,8 @@
 New-Item -type file [ファイル名]
 
+sudo apt install pkg-config
+sudo apt-get install libssl-dev
+
 github
 https://github.com/ghmagazine/rustbook
 https://github.com/forcia/rustbook
--- Rust_workspace/rust_basics/basic/chapter03/forcia/basic_trait/target/debug/basic_trait.d
+++ Rust_workspace/rust_basics/basic/chapter03/forcia/basic_trait/target/debug/basic_trait.d
@@ -1,1 +1,1 @@
-E:\000_GitHub\workspace\Rust_workspace\rust_basics\basic\basic_trait\target\debug\basic_trait.exe: E:\000_GitHub\workspace\Rust_workspace\rust_basics\basic\basic_trait\src\main.rs
+/home/ytakasugi/000_learning/workspace/Rust_workspace/rust_basics/basic/chapter03/forcia/basic_trait/target/debug/basic_trait: /home/ytakasugi/000_learning/workspace/Rust_workspace/rust_basics/basic/chapter03/forcia/basic_trait/src/main.rs
