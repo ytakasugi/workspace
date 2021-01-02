e1774329d0dfb0d0a5b7885edae5503defd1754c
ytakasugi <sh7.tibi0129@gmail.com>
2021-01-03 00:35:10 +0900

    add code

--- Rust_workspace/rust_basics/basic/chapter03/rust_bicycle_book/bitonic-sorter/src/second.rs
+++ Rust_workspace/rust_basics/basic/chapter03/rust_bicycle_book/bitonic-sorter/src/second.rs
@@ -1,13 +1,22 @@
-pub fn sort(x: &mut [u32], up: bool) {
+use super::SortOrder;
+
+pub fn sort<T: Ord>(x: &mut [T], order: &SortOrder) {
+    match *order {
+        SortOrder::Ascending => do_sort(x, true),
+        SortOrder::Descending => do_sort(x, false),
+    };
+}
+
+fn do_sort<T: Ord>(x: &mut [T], up: bool) {
     if x.len() > 1 {
         let mid_point = x.len() / 2;
-        sort(&mut x[..mid_point], true);
-        sort(&mut x[mid_point..], false);
+        do_sort(&mut x[..mid_point], true);
+        do_sort(&mut x[mid_point..], false);
         sub_sort(x, up);
     }
 }
 
-fn sub_sort(x: &mut [u32], up: bool) {
+fn sub_sort<T: Ord>(x: &mut [T], up: bool) {
     if x.len() > 1 {
         compare_and_swap(x, up);
         let mid_point = x.len() / 2;
@@ -16,7 +25,7 @@ fn sub_sort(x: &mut [u32], up: bool) {
     }
 }
 
-fn compare_and_swap(x: &mut [u32], up: bool) {
+fn compare_and_swap<T: Ord>(x: &mut [T], up: bool) {
     let mid_point = x.len() / 2;
     for i in 0..mid_point {
         if (x[i] > x[mid_point + i]) == up {
@@ -30,6 +39,7 @@ fn compare_and_swap(x: &mut [u32], up: bool) {
 mod tests {
     // 親モジュール（first）のsort関数を使用する
     use super::sort;
+    use crate::SortOrder::*;
 
     // #[test]の付いた関数はcargo testとしたときに実行される
     #[test]
@@ -40,7 +50,7 @@ mod tests {
         let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
 
         // xのスライスを作成し、sort関数を呼び出す
-        sort(&mut x, true);
+        sort(&mut x, &Ascending);
 
         // xの要素が昇順にソートされていることを確認する
         assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330]);
@@ -49,7 +59,7 @@ mod tests {
     #[test]
     fn sort_u32_descending() {
         let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
-        sort(&mut x, false);
+        sort(&mut x, &Descending);
         // xの要素が降順にソートされていることを確認する
         assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
     }
@@ -58,14 +68,14 @@ mod tests {
     fn sort_str_ascending() {
         // 文字列のベクタを作成し、ソートする
         let mut x = vec!["Rust", "is", "fast", "and", "memory-efficient", "with", "no", "GC"];
-        sort(&mut x, true);
+        sort(&mut x, &Ascending);
         assert_eq!(x, vec!["GC", "Rust", "and", "fast", "is", "memory-efficient", "no", "with"]);
     }
 
     #[test]
     fn sort_str_descending() {
         let mut x = vec!["Rust", "is", "fast", "and", "memory-efficient", "with", "no", "GC"];
-        sort(&mut x, false);
-        assert_eq!(x, vec!["Rust", "is", "fast", "and", "memory-efficient", "with", "no", "GC"]);
+        sort(&mut x, &Descending);
+        assert_eq!(x, vec!["with", "no", "memory-efficient", "is", "fast", "and", "Rust", "GC"]);
     }
 }
---
+++ Rust_workspace/rust_basics/basic/chapter03/rust_bicycle_book/bitonic-sorter/target/rls/debug/deps/bitonic_sorter-cbdd0c160cf4984b.d
@@ -0,0 +1,7 @@
+/home/ytakasugi/000_learning/workspace/Rust_workspace/rust_basics/basic/chapter03/rust_bicycle_book/bitonic-sorter/target/rls/debug/deps/bitonic_sorter-cbdd0c160cf4984b.rmeta: src/lib.rs src/first.rs src/second.rs
+
+/home/ytakasugi/000_learning/workspace/Rust_workspace/rust_basics/basic/chapter03/rust_bicycle_book/bitonic-sorter/target/rls/debug/deps/bitonic_sorter-cbdd0c160cf4984b.d: src/lib.rs src/first.rs src/second.rs
+
+src/lib.rs:
+src/first.rs:
+src/second.rs:
