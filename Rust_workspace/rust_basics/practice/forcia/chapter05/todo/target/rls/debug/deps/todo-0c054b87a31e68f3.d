1338fc331611a9ac240e9722b170fa0aaa76455e
ytakasugi <sh7.tibi0129@gmail.com>
2021-01-05 21:45:01 +0900

    code fix

---
+++ Rust_workspace/rust_basics/practice/forcia/chapter05/todo/target/rls/debug/deps/todo-0c054b87a31e68f3.d
@@ -0,0 +1,6 @@
+/home/ytakasugi/000_learning/workspace/Rust_workspace/rust_basics/practice/forcia/chapter05/todo/target/rls/debug/deps/todo-0c054b87a31e68f3.rmeta: src/main.rs /home/ytakasugi/000_learning/workspace/Rust_workspace/rust_basics/practice/forcia/chapter05/todo/templates/index.html
+
+/home/ytakasugi/000_learning/workspace/Rust_workspace/rust_basics/practice/forcia/chapter05/todo/target/rls/debug/deps/todo-0c054b87a31e68f3.d: src/main.rs /home/ytakasugi/000_learning/workspace/Rust_workspace/rust_basics/practice/forcia/chapter05/todo/templates/index.html
+
+src/main.rs:
+/home/ytakasugi/000_learning/workspace/Rust_workspace/rust_basics/practice/forcia/chapter05/todo/templates/index.html:
--- Rust_workspace/rust_basics/practice/forcia/chapter05/todo/target/rls/debug/deps/todo-6e489b599d7ebf83.d
+++
@@ -1,6 +0,0 @@
-/home/ytakasugi/000_learning/workspace/Rust_workspace/rust_basics/practice/forcia/chapter05/todo/target/rls/debug/deps/todo-6e489b599d7ebf83.rmeta: src/main.rs /home/ytakasugi/000_learning/workspace/Rust_workspace/rust_basics/practice/forcia/chapter05/todo/templates/index.html
-
-/home/ytakasugi/000_learning/workspace/Rust_workspace/rust_basics/practice/forcia/chapter05/todo/target/rls/debug/deps/todo-6e489b599d7ebf83.d: src/main.rs /home/ytakasugi/000_learning/workspace/Rust_workspace/rust_basics/practice/forcia/chapter05/todo/templates/index.html
-
-src/main.rs:
-/home/ytakasugi/000_learning/workspace/Rust_workspace/rust_basics/practice/forcia/chapter05/todo/templates/index.html:
