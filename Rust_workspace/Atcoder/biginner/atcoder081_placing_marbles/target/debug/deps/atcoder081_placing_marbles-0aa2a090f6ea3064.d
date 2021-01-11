b25dac63ba15d0aafebb046157217012215b6cf4
ytakasugi <sh7.tibi0129@gmail.com>
2021-01-11 10:48:35 +0900

    change files

---
+++ Rust_workspace/Atcoder/biginner/atcoder081_placing_marbles/target/debug/deps/atcoder081_placing_marbles-0aa2a090f6ea3064.d
@@ -0,0 +1,5 @@
+/home/ytakasugi/000_learning/workspace/Rust_workspace/Atcoder/biginner/atcoder081_placing_marbles/target/debug/deps/atcoder081_placing_marbles-0aa2a090f6ea3064: src/main.rs
+
+/home/ytakasugi/000_learning/workspace/Rust_workspace/Atcoder/biginner/atcoder081_placing_marbles/target/debug/deps/atcoder081_placing_marbles-0aa2a090f6ea3064.d: src/main.rs
+
+src/main.rs:
---
+++ Rust_workspace/Atcoder/biginner/atcoder081_placing_marbles/target/rls/debug/deps/atcoder081_placing_marbles-9f50f0a8b47146da.d
@@ -0,0 +1,5 @@
+/home/ytakasugi/000_learning/workspace/Rust_workspace/Atcoder/biginner/atcoder081_placing_marbles/target/rls/debug/deps/atcoder081_placing_marbles-9f50f0a8b47146da.rmeta: src/main.rs
+
+/home/ytakasugi/000_learning/workspace/Rust_workspace/Atcoder/biginner/atcoder081_placing_marbles/target/rls/debug/deps/atcoder081_placing_marbles-9f50f0a8b47146da.d: src/main.rs
+
+src/main.rs:
