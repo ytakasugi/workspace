b0aff2de253eb0a4db65952767d0ed37409f7314
ytakasugi <sh7.tibi0129@gmail.com>
2021-01-23 21:57:26 +0900

    rename dir

---
+++ Rust_workspace/Atcoder/Contest/beginner/b189_alcoholic/target/rls/debug/deps/b189_alcoholic-466628d7236bf496.d
@@ -0,0 +1,5 @@
+/home/ytakasugi/000_learning/workspace/Rust_workspace/Atcoder/Contest/beginner/b189_alcoholic/target/rls/debug/deps/b189_alcoholic-466628d7236bf496.rmeta: src/main.rs
+
+/home/ytakasugi/000_learning/workspace/Rust_workspace/Atcoder/Contest/beginner/b189_alcoholic/target/rls/debug/deps/b189_alcoholic-466628d7236bf496.d: src/main.rs
+
+src/main.rs:
---
+++ Rust_workspace/Atcoder/Contest/beginner/b189_alcoholic/target/rls/debug/deps/b189_alcoholic-6dbe5b4faa87f0ed.d
@@ -0,0 +1,5 @@
+/home/ytakasugi/000_learning/workspace/Rust_workspace/Atcoder/Contest/beginner/b189_alcoholic/target/rls/debug/deps/b189_alcoholic-6dbe5b4faa87f0ed.rmeta: src/main.rs
+
+/home/ytakasugi/000_learning/workspace/Rust_workspace/Atcoder/Contest/beginner/b189_alcoholic/target/rls/debug/deps/b189_alcoholic-6dbe5b4faa87f0ed.d: src/main.rs
+
+src/main.rs:
