210762984f67557630af4eab303708b385d357c7
ytakasugi <sh7.tibi0129@gmail.com>
2021-03-20 00:05:15 +0900

    new cargo project

---
+++ Rust_workspace/RPL/section10/section1001/section1001_01/target/debug/deps/section1001_01-968583445023db47.d
@@ -0,0 +1,5 @@
+/home/ytakasugi/000_learning/workspace/Rust_workspace/RPL/section10/section1001/section1001_01/target/debug/deps/section1001_01-968583445023db47: src/main.rs
+
+/home/ytakasugi/000_learning/workspace/Rust_workspace/RPL/section10/section1001/section1001_01/target/debug/deps/section1001_01-968583445023db47.d: src/main.rs
+
+src/main.rs:
